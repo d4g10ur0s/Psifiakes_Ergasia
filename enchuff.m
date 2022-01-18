576da44b119658664736ce0f384e590ca6131351
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2022-01-18 20:06:25 +0200

    Add files via upload

--- enchuff.m
+++ enchuff.m
@@ -1,12 +1,11 @@
 function enc = enchuff(dict,mstr)
+    %to dict sthn prwth grammh stoixeia alphavitou , sthn 2h to code
     %prepei na parw ka8e element tou mstr ke na vrw ti code einai mesa sto
-    %dict
     enc = "";
-    disp(mstr{1});
     lngt = strlength(mstr);
     for i = 1:lngt
         for j = 1:numel(dict(1,:))
-            if mstr{1}(i) == dict(1,j)
+            if mstr(1,i) == dict(1,j)
                 enc = append(enc,dict(2,j));
             end
         end
