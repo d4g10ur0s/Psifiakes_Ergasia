59ead35c524dc586da4c28857cfb26789dd218ad
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2022-01-20 14:15:46 +0200

    Add files via upload

--- lmax.m
+++ lmax.m
@@ -1,4 +1,5 @@
 function [xq, D] = lmax(x, N, min_value, max_value)
+    x = randn(10000,1);%random x 
     pcount = 2^N;%to plh8os twn shmeiwn pou prepei na xwrhsw
     %pairnw tyxaia shmeia kvantismou
     mrange = (max_value - min_value);%orizw step to range
@@ -14,25 +15,20 @@ function [xq, D] = lmax(x, N, min_value, max_value)
     for i = 2:(pcount-1)%prepei na vrw limits
         limits(i) = ( tskv(i) + tskv(i-1) )/2;%ypologismos kentroeidwn
     end
-    xq = zeros(5,1);
-    x = rand(5,1) *365;
+    xq = zeros(10000,1)*100;
     for i=1:length(x)%gia ka8e timh sto x 
         for j=2:pcount
             if ((x(i)>limits(j-1))&&(x(i)<=limits(j)) || x(i) < limits(j)) %euresh diasthmatos pou anhkei to x
-                xq(i) = j-1; 
+                xq(i) = pcount-j+1; 
                 break;
             end 
             if (j==pcount)
-                disp("edw eimai");
-                xq(i) = 2^N;
+                xq(i) = 1;
             end
         end
     end
-    disp(limits(2));
-    disp((tskv(2)+tskv(1))/2);
-    disp(xq);
-    disp(x(1));
-    maxIterations = 20;%to polu 20 iterations
+    %disp(limits);
+    maxIterations = 10;%to polu 20 iterations
     k = 1;
     D = zeros(maxIterations,1);
     T = zeros(pcount,1);
@@ -44,23 +40,24 @@ function [xq, D] = lmax(x, N, min_value, max_value)
         k = k + 1;
         for i =2:pcount-2
             T(i) = (limits(i)+limits(i-1))/2;
+            limits(i) = T(i);
         end
-
         for i=1:length(x)%gia ka8e timh sto x 
             for j=2:pcount
                 if ((x(i)>limits(j-1))&&(x(i)<=limits(j)) || x(i) < limits(j)) %euresh diasthmatos pou anhkei to x
-                    xq(i) = j-1; 
+                    xq(i) = pcount-j+1; 
                     break;
                 end 
                 if (j==pcount)
-                    disp("edw eimai");
-                    xq(i) = 2^N;
+                    xq(i) = 1;%to megalutero epipedo kvadishs
                 end
             end
         end
         D(k) = sum((x-xq).^2);%ypologismos paramorfwshs
         if (abs(D(k-1)-D(k))<10^(-6)) 
+            disp( abs( D(k-1) - D(k) ) );
             break;
         end
     end
+    disp("SQNR : " + mean( x.^2 ) / mean( (x-xq).^2 ) );
 end
