0a3d53d0b983b307db0fecd3e9a3818c29d9fbfb
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2022-01-22 16:38:36 +0200

    Add files via upload

--- kmeans_quant.m
+++ kmeans_quant.m
@@ -1,12 +1,11 @@
 function [xq, D] = kmeans_quant(x, N)
     x = randn(10000,10000);
-    %x = randn(10000,1);
-    mb = 1;
-    ma = ones(10000,1);
-    for i=1:10000
-        ma(i,1) = ma(i,1)/i;
-    end
-    x = filter(mb,ma,x);
+    %mb = 1;
+    %ma = ones(10000,1);
+    %for i=1:10000
+    %    ma(i,1) = ma(i,1)/i;
+    %end
+    %x = filter(mb,ma,x);
 
     xq = zeros(size(x));
     [idx , centers] = kmeans(x,2^N);%exw parei ta kentroeidh
@@ -16,25 +15,28 @@ function [xq, D] = kmeans_quant(x, N)
     k = 1;
     maxIterations = 20;%to polu 20 iterations
     D = zeros(maxIterations,1);
-    D(k) = sum((x-xq).^2,'all');%ypologismos paramorfwshs
+    D(k) = D(k) + sum( (x(i,:)-xq(i,:)).^2);%ypologismos paramorfwshs
+    D(k) = D(k) / 10000;
      while k < maxIterations
         %arxika ypologizw nea kentroeidh
         k = k + 1;
         [idx , centers] = kmeans(xq,2^N);
-        for i = 1:length(x(:,1))
+        for i = 1:10000
             xq(i,:) = centers(idx(i),:);
         end
         for i=1:10000
-            D(k) = D(k) + sum( (x(1,:)-xq(i,:)).^2 ,'all');%ypologismos paramorfwshs
+            D(k) = D(k) + sum( (x(i,:)-xq(i,:)).^2);%ypologismos paramorfwshs
         end
-        D(k) = D(k)/100;
-        if (abs(D(k-1)-D(k))<10^(-6)) 
+        D(k) = D(k)/10000;
+        if (abs(D(k-1)-D(k))<10^(-16)) 
             break;
         end
      end
+     disp(k);
      sqnr = 0;
      for i=1:10000
          sqnr = sqnr + mean( x(i,:).^2 ) / mean( (x(i,:)-xq(i,:)).^2 ) ;
      end
      disp("SQNR : " + log10(sqnr/10000)*10);
+     disp("MSE : " + immse(x,xq));
 end
